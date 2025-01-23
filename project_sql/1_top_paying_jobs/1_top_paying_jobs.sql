01f3ea393ee75db81131e33caaf3358180bead85
Ziad-AZ <ziad.azekriti@gmail.com>
2025-01-23 20:53:34 +0100

    Question 1(highest paying jobs)

---
+++ project_sql/1_top_paying_jobs/1_top_paying_jobs.sql
@@ -0,0 +1,21 @@
+/* Question: What are the top paying jobs as a Data Analyst
+Top 10 paying jobs.
+Only jobs with mentioned salaries and that are remotely.
+*/
+SELECT 
+job_id,
+job_title,
+company_dim.name AS company_name,
+job_location,
+job_schedule_type,
+salary_year_avg
+FROM
+job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+job_title_short = 'Data Analyst'AND
+job_location = 'Anywhere'AND 
+salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10;
+
