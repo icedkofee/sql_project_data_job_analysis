c78564a12ac9c668aac9058a09d71ad389c27d89
Ziad-AZ <ziad.azekriti@gmail.com>
2025-02-21 16:59:49 +0100

    top_paying_skills and fix top_in_demand

---
+++ project_sql/3_top_in_demand_skills_all_time/3_top_in_demand_skills_all_time.sql
@@ -0,0 +1,16 @@
+/* Top 10 in demand skills all time for data analyts*/
+
+SELECT  
+COUNT (skills_dim.skills) as skill_demand,
+skills_dim.skills
+FROM job_postings_fact
+
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+where 
+job_title_short = 'Data Analyst' AND
+
+job_postings_fact.salary_year_avg is NOT NULL
+GROUP BY skills_dim.skills
+ORDER BY skill_demand DESC
+limit 10;
--- project_sql/4_top_skill_per_salary/4_top_skill_per_salary.sql
+++ project_sql/4_top_skill_per_salary/4_top_skill_per_salary.sql
@@ -1,16 +1,15 @@
-/* Top 10 paying skills all time */
+/* Top 10 paying skills all time for data science and data analysis*/
 
 SELECT  
-COUNT (skills_dim.skills) as skill_demand,
+ROUND(AVG(job_postings_fact.salary_year_avg), 2) AS AVG_salary_per_skill,
 skills_dim.skills
 FROM job_postings_fact
 
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 where 
-job_title_short = 'Data Analyst' AND
-
+(job_title_short = 'Data Analyst' OR job_title_short = 'Data Scientist') AND
 job_postings_fact.salary_year_avg is NOT NULL
 GROUP BY skills_dim.skills
-ORDER BY skill_demand DESC
+ORDER BY AVG_salary_per_skill DESC
 limit 10;
